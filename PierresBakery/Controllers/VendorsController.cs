2303eb11656362e9ffa45e1ccd96a62e8c52a3c6
Benjamin Thom <jaminthom@gmail.com>
2020-03-13 14:30:16 -0700

    update Order class to include order title and add property instances wthin all necessary files

--- PierresBakery/Controllers/VendorsController.cs
+++ PierresBakery/Controllers/VendorsController.cs
@@ -30,11 +30,11 @@ namespace PierresBakery.Controllers
     // This one creates new Orders within a given Vendor, not new Vendors:
 
     [HttpPost ("/vendors/{vendorId}/orders")]
-    public ActionResult Create (int vendorId, string orderDescription)
+    public ActionResult Create (int vendorId, string orderTitle, string orderDescription)
     {
       Dictionary<string, object> model = new Dictionary<string, object> ();
       Vendor foundVendor = Vendor.Find (vendorId);
-      Order newOrder = new Order (orderDescription);
+      Order newOrder = new Order (orderTitle, orderDescription);
       foundVendor.AddItem (newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add ("orders", vendorOrders);
